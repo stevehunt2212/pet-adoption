8e6e97b2046ef422293e5bc9cf698ce81360192b
Steve <stevehunt2212@gmail.com>
2024-06-19 15:02:29 +0100

    Added dynamic temperature feature

---
+++ main.js
@@ -0,0 +1,7 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector('#our-temp').textContent = ourTemperature
+}
+start()
